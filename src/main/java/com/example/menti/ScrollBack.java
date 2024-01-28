aedb3410b9e7b29059d26b228fddf63c32b5cb3c
Andy Pak <im.andy.pak@gmail.com>
2024-01-28 12:57:18 -0500

    fix path for music files and image files

--- src/main/java/com/example/menti/ScrollBack.java
+++ src/main/java/com/example/menti/ScrollBack.java
@@ -23,9 +23,9 @@ public class ScrollBack extends JPanel implements ActionListener {
     int move = 20;
     BufferedImage buffer; // Off-screen image for double buffering
 
-    static String music = "Audio\\mindfulness-relaxation-amp-meditation-music-22174.wav";
+    static String music = "medSource/mindfulness-relaxation-amp-meditation-music-22174.wav";
 
-    String open = "Audio\\Open Your Eyes.wav";
+    String open = "medSource/src_medSource_Open Your Eyes.wav";
     long startTime; // Start time for countdown
     long countdownDuration; // Countdown duration in milliseconds
     JLabel countdownLabel; // Label to display countdown
@@ -33,7 +33,7 @@ public class ScrollBack extends JPanel implements ActionListener {
     JButton startButton; // Button to start countdown
 
     public ScrollBack() {
-        ImageIcon obj = new ImageIcon("/Users/kennychen/Desktop/IdeaProjects/Menti/src/medSource/waves_1.png");
+        ImageIcon obj = new ImageIcon("medSource/waves_1.png");
         pic = obj.getImage();
         picWidth = pic.getWidth(null);
         picHeight = pic.getHeight(null);
